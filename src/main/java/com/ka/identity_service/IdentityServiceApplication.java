f7c59ec8c507cabfa20d87752fa73277711fc129
Ka <kaakaa300@gmail.com>
2025-12-21 05:09:55 +0700

    chore: add rule of commit message in app file

--- src/main/java/com/ka/identity_service/IdentityServiceApplication.java
+++ src/main/java/com/ka/identity_service/IdentityServiceApplication.java
@@ -11,3 +11,14 @@ public class IdentityServiceApplication {
 	}
 
 }
+//| type         | Ý nghĩa                                                         |
+//| ------------ | --------------------------------------------------------------- |
+//| feat         | Thêm tính năng mới                                              |
+//| fix          | Sửa bug                                                         |
+//| docs         | Cập nhật tài liệu (README…)                                     |
+//| style        | Thay đổi format, không ảnh hưởng logic (format code, prettier…) |
+//| refactor     | Tái cấu trúc code, không sửa bug / không thêm tính năng         |
+//| perf         | Tối ưu hiệu năng                                                |
+//| test         | Thêm / sửa test                                                 |
+//| chore        | Việc linh tinh (config, build, CI/CD…)                          |
+//| revert       | Rollback commit                                                 |
